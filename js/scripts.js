d79b281c1c03561b51eddd3c5f9bb4ecdbf0cc67
Jared Farkas and mustafa ghasemi <jared@>
2019-04-04 09:28:19 -0700

    finish the logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,71 @@
 
 
-function pigLatin(userInput) {
-
-  var phrase = userInput.split(" ");
-  console.log(phrase);
+function roman(num) {
+if(num >3999){
+  return "We can't convert it.";
+}
   var output = "";
-  for (var i = 0; i < phrase.length; i++) {
-    var word = phrase[i].split("");
-    if (word[0]==="a" || word[0] ==="e" || word[0]==="i" || word[0]==="o" || word[0]==="u"){
-      output += word.join("") + "ay ";
-    } else {
-      console.log(word);
-      for (var j = 0; j < word.length; j++) {
-        var firstLetter = word.shift();
-        word.push(firstLetter);
-        //console.log(word);
-        if (word[0]==="a" || word[0] ==="e" || word[0]==="i" || word[0]==="o" || word[0]==="u"){
-          output += word.join("") + "ay ";
-          break;
-        }
-      }
-    }
-
-
+  while(num >= 1000){
+    num -= 1000;
+    output += "M"
   }
-  return output;
+  while(num >= 900){
+    num -= 900;
+    output += "CM";
+  }
+  while(num >= 500){
+    num -= 500;
+    output += "D"
+  }
+  while(num >= 400){
+    num -= 400;
+    output += "CD";
+  }
+  while(num >= 100){
+    num -= 100;
+    output += "C"
+  }
+  while(num >= 90){
+    num -= 90;
+    output += "XC";
+  }
+  while(num >= 50){
+    num -= 50;
+    output += "L"
+  }
+  while(num >= 40){
+    num -= 40;
+    output += "XL";
+  }
+  while(num >= 10){
+    num -= 10;
+    output += "X"
+  }
+  while(num >= 9){
+    num -= 9;
+    output += "IX";
+ }
+  while(num >= 5){
+    num -= 5;
+    output += "V";
+  }
+  while(num >= 4){
+    num -= 4;
+    output += "IV";
+  }
+  while(num >= 1){
+    num -= 1;
+    output += "I";
+  }
 
+return output;
 }
 
 $(document).ready(function(){
   $("form#pig-latin").submit (function(event){
     event.preventDefault();
     var userInput = $("input#latin").val();
-    var phrase =pigLatin(userInput);
+    var phrase =roman(userInput);
 
     $("#result").text(phrase);
 
